9bdd6f5d9bf0742dc23bb84770c8890f8436c3c2
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2022-09-19 21:26:54 +0300

    Create MinimumSizeSubarraySum.java

---
+++ MinimumSizeSubarraySum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        int left = 0;
+        int total = 0;
+        int res = nums.length + 1;
+        for (int i = 0; i <  nums.length; i++){
+            total += nums[i];
+            while (total >= target){
+                res =  Math.min(i - left + 1, res);
+                total -= nums[left];
+                left += 1;
+                
+            }
+        }
+        if (res == nums.length + 1) return 0;
+        else return res;
+    }
+}
