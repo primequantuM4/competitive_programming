8f51d6eca52bca05139e730d14b1d1cfc9f0133d
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2022-09-20 11:25:49 +0300

    Update ProductofArrayExceptSelf.java

--- ProductofArrayExceptSelf.java
+++ ProductofArrayExceptSelf.java
@@ -1,16 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int leftptr = 1;
-        int rightptr = 1;
+        int leftptr = 1; 
+        int rightptr = 1; 
         int[] ans = new int[nums.length];
+        int[] left = new int[nums.length]; 
+        int[] right = new int[nums.length];
         for (int i = 0; i < nums.length; i++){
-            ans[i] = leftptr;
+            left[i] = leftptr;
             leftptr *= nums[i];
         }
         for (int i = nums.length - 1; i >= 0; i--){
-            ans[i] *= rightptr;
+            right[i] = rightptr;
             rightptr *= nums[i];
+        }
+        for (int i = 0; i < nums.length; i++){
+            ans[i] = left[i] * right[i];
         }
         return ans;
     }
 }
+
