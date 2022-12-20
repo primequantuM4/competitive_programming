3feb4b032ca827ce61d556cdef9c8e561a18505a
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2022-12-20 17:47:18 +0300

    Create word_order.py

---
+++ word_order.py
@@ -0,0 +1,18 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+number = int(input())
+arr = [input() for i in range(number)]
+total_num = []
+res = set()
+dic = {}
+for i in arr:
+    if i not in res: 
+        total_num.append(1)
+        res.add(i)
+        dic[i] = len(total_num) - 1
+    else:
+        idx = dic.get(i)
+        total_num[idx] += 1
+
+print(len(res))
+for i in total_num:
+    print(i, end=' ')
