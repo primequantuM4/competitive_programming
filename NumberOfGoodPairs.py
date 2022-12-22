d1928a6bd5d8af6b72507d3c38412035f788ab0e
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2022-12-22 19:45:45 +0300

    Update NumberOfGoodPairs.py

--- NumberOfGoodPairs.py
+++ NumberOfGoodPairs.py
@@ -1,20 +1,9 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        #approach -> use a hashmap to store a key value pair of numbers and arr
-        #when we use arr we can know the number of times that each element has in the array
-        number_mapping = {}
-        number_values = []
-        for i in range(len(nums)):
-            if nums[i] not in number_mapping:
-                number_mapping[nums[i]] = [i]
-                number_values.append(nums[i])
-            else:
-                number_mapping[nums[i]].append(i)
-            
-        res = 0
-        for i in number_values:
-            res += (len(number_mapping[i]) * (len(number_mapping[i]) - 1)) // 2
-        return res
-
-        
-
+        #approach -> use a hashmap to store a key value pair of numbers and it's count
+        #Then use a simple mathematics calculation to find how many pairs it can have
+        number_mapping = Counter(nums)
+        pairs = 0
+        for values in number_mapping.values():
+            pairs += (values * (values - 1)) // 2
+        return pairs
