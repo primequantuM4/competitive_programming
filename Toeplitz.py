2e4fb698c44b63961fdd43b22d3dc966069ee07b
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-01-22 20:14:51 +0300

    Create Toeplitz.py

---
+++ Toeplitz.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if row > 0 and col > 0 and matrix[row][col] != matrix[row-1][col-1]:
+                    return False
+        return True
