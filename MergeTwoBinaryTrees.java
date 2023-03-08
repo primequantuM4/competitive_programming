369dbb23a67e53ccf0680d38e9e8a8f18e3143fe
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-03-08 16:53:59 +0300

    Create MergeTwoBinaryTrees.java

---
+++ MergeTwoBinaryTrees.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        else if (root2 == null) return root1;
+
+        TreeNode left = mergeTrees(root1.left, root2.left);
+        TreeNode right = mergeTrees(root1.right, root2.right);
+
+        root1.val += root2.val;
+        root1.left = left;
+        root1.right = right; 
+
+        return root1;
+    }
+}
