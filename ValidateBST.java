c0e36038f1c53175812d9dc4e0f0a13c3e81f034
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-03-08 13:08:15 +0300

    Create ValidateBST.java

---
+++ ValidateBST.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    long prev = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        //do in order traversal
+        return inorder(root);
+    }
+    public boolean inorder(TreeNode root){
+        if (root == null) return true;
+
+        boolean traversed = inorder(root.left);
+        if (prev >= root.val || !traversed) return false;
+        prev = root.val;
+        return inorder(root.right);
+        
+    }
+}
