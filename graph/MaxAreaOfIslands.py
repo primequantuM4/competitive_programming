58f6595da640f645209e9d09c6db0ec4e7c6cd61
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-04-09 21:59:14 +0300

    Update MaxAreaOfIslands.py

--- graph/MaxAreaOfIslands.py
+++ graph/MaxAreaOfIslands.py
@@ -1,24 +1,35 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        inBound = lambda r, c: 0 <= r < len(grid) and 0 <= c < len(grid[0])
-        visited = set()
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
-        max_area = 0
+        maxArea = 0
+
+        def dfs(row, col):
+            stack = []
+            stack.append((row, col))
+
+            area = 1
+            grid[row][col] = 2
+            inBound = lambda r, c: 0 <= r< len(grid) and 0 <= c < len(grid[0])
+
+            directions = [(1,0), (0,1), (-1,0), (0,-1)]
+
+            while stack:
+                cur_row, cur_col = stack.pop()
+
+                for r, c in directions:
+                    next_row = cur_row + r
+                    next_col = cur_col + c
+                    isLand = inBound(next_row, next_col) and grid[next_row][next_col] == 1
+                    
+                    if isLand:
+                        grid[next_row][next_col] = 2
+                        area += 1
+                        stack.append((next_row, next_col))
+
+            return area
+
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                if grid[row][col] == 1 and (row, col) not in visited:
-                    cur_area = 0
-                    stack = [(row, col)]
-                    visited.add((row, col))
-                    while stack:
-                        rows, cols = stack.pop()
-                        cur_area += 1
-                        for r, c in directions:
-                            next_row = r + rows
-                            next_col = c + cols
-                            seen = (next_row, next_col) in visited
-                            if inBound(next_row, next_col) and (not seen) and grid[next_row][next_col] == 1:
-                                stack.append((next_row, next_col))
-                                visited.add((next_row, next_col))
-                    max_area = max(cur_area, max_area)
-        return max_area
+                if grid[row][col] == 1:
+                    maxArea = max(maxArea, dfs(row, col))
+
+        return maxArea
