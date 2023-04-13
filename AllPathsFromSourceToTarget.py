1a5ba6b7f3e4d9d7901ef48d7faaf36857a1323c
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-04-13 12:49:53 +0300

    Create AllPathsFromSourceToTarget.py

---
+++ AllPathsFromSourceToTarget.py
@@ -0,0 +1,22 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        #goal_node = len(graph) - 1
+        #adj list maybe but not necessary
+        goal_node = len(graph) - 1
+
+        stack = [(0, [0])]
+        paths = []
+
+        while stack:
+            cur_node, current_path = stack.pop()
+
+            if cur_node == goal_node:
+                paths.append(current_path[:])
+
+            for neighbor in graph[cur_node]:
+                current_path.append(neighbor)
+                stack.append((neighbor, current_path[:]))
+
+                current_path.pop()
+
+        return paths
