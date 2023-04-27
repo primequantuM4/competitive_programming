0502c61fdced7428e09a5897ee9c73aaa648c841
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-04-27 18:27:11 +0300

    Create AverageOfLevelsInBinaryTree.py

---
+++ AverageOfLevelsInBinaryTree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        if not root:
+            return []
+        averageArray = []
+        queue = deque([root])
+
+        while queue:
+            length = len(queue)
+            value = 0
+            for _ in range(length):
+                cur_node = queue.popleft()
+                value += cur_node.val
+
+                if cur_node.left:
+                    queue.append(cur_node.left)
+
+                if cur_node.right:
+                    queue.append(cur_node.right)
+
+            averageVal = value / length
