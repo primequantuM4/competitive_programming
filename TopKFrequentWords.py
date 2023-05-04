b615458aca12537368f38544aa06376b5fc43edf
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-05-04 17:44:21 +0300

    Create TopKFrequentWords.py

---
+++ TopKFrequentWords.py
@@ -0,0 +1,16 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        words_count = defaultdict(int)
+        kFrequencies, heapified_words = [], []
+        for i in words:
+            words_count[i] -= 1
+        
+        for word, frequency in words_count.items():
+            heappush(heapified_words, (frequency, word))
+
+        for _ in range(k):
+            print(heapified_words)
+            _, word = heappop(heapified_words)
+            kFrequencies.append(word)
+
+        return kFrequencies
