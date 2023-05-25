71d1b2abb88ad6d5e110199e6723608fe782db74
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-05-25 18:05:37 +0300

    Update lemonade-change.py

--- lemonade-change.py
+++ lemonade-change.py
@@ -1,27 +1,24 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        #get a running sum
-        #return true if the sum is positive? and false if it's negative...
-        bill_count = defaultdict(int)
-        print(len(bills))
-        for idx,bill in enumerate(bills):
+        five_bills, ten_bills = 0, 0
+        for bill in bills:
             if bill == 5:
-                bill_count[5] += 1
+                five_bills += 1
             
             elif bill == 10:
-                if bill_count[5] == 0:
+                if not five_bills:
                     return False
                 
-                bill_count[5] -= 1
-                bill_count[10] += 1
+                five_bills -= 1
+                ten_bills += 1
 
             elif bill == 20:
-                if bill_count[10] >= 1 and bill_count[5] >= 1:
-                    bill_count[10] -= 1
-                    bill_count[5] -= 1
+                if ten_bills and five_bills:
+                    ten_bills -= 1
+                    five_bills -= 1
 
-                elif bill_count[5] >= 3:
-                    bill_count[5] -= 3
+                elif five_bills >= 3:
+                    five_bills -= 3
 
                 else:
                     return False
