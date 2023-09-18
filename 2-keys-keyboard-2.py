d648f57c3c6f540c97424eff829dc049ce548349
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-09-18 16:14:35 +0300

    Update 2-keys-keyboard-2.py

--- 2-keys-keyboard-2.py
+++ 2-keys-keyboard-2.py
@@ -1,12 +1,24 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        ans = 0
-        d = 2
-        while n > 1:
-            while n % d == 0:
-                ans += d
-                n /= d
+        @cache
+        def dp(buffer_size):
+            if buffer_size == 1:
+                return 0
+            value = buffer_size #considering one to be a factor
+            for num in self.get_factors(buffer_size):
+                paste_buffer_step = int(buffer_size / num - 1)
+                copy_step = 1
+                steps = dp(num)
+                value = min(value, steps + paste_buffer_step + copy_step)
+            return value
+        return dp(n) 
+            
+    def get_factors(self, n):
+        factors = []
+        for factor in range(1, n // 2):
+            if n % factor == 0:
+                factors.append(factor)
+        return factors
 
-            d += 1
+
         
-        return ans
