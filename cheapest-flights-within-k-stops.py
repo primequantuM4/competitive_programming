9893d473e3e167364901ee46f13f431d50161674
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-10-06 16:25:13 +0300

    Update cheapest-flights-within-k-stops.py

--- cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops.py
@@ -1,59 +1,23 @@
-from collections import defaultdict, deque
-import heapq
-from typing import List
-
-## Dijkstra
 class Solution:
-    def findCheapestPrice(self, n, flights, src, dst, k):
-        visited = {}
-        adj = defaultdict(list)
-        for s, d, p in flights:
-            adj[s].append((d, p))
-        pq = [(0, 0, src)]
-        while pq:
-            cost, stops, node = heapq.heappop(pq)
-            if node == dst and stops - 1 <= k:
-                return cost
-            if node not in visited or visited[node] > stops:
-                visited[node] = stops
-                for neighbor, price in adj[node]:
-                    heapq.heappush(pq, (cost + price, stops + 1, neighbor))
-        return -1
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj_list = defaultdict(list)
+        shortest = defaultdict(lambda:inf)
+        for cur, to, price in flights:
+            adj_list[cur].append((to, price))
+        
+        heap = [(0,0, src)]
+        visited = defaultdict(int)
+        visited[src] = 0
 
 
-## BFS solution
-class Solution:
-    def findCheapestPrice(self, n, flights, src, dst, k):
-        adj = defaultdict(list)
-        for s, d, p in flights:
-            adj[s].append((d, p))
-        queue = deque()
-        queue.append((src, 0))
-        costs = [float("inf")] * n
-        while queue and k >= 0:
-            m = len(queue)
-            for _ in range(m):
-                curr, cost = queue.popleft()
-                for node, price in adj[curr]:
-                    if cost + price < costs[node]:
-                        costs[node] = cost + price
-                        queue.append((node, costs[node]))
-            k -= 1
-        return costs[dst] if costs[dst] != float("inf") else -1
+        while heap:
+            k_stop,price,node = heappop(heap)
+            if k_stop > k:
+                break
+            for next_node, next_price in adj_list[node]:
+                if next_node not in visited or visited[next_node] > price + next_price:
+                    visited[next_node] = price + next_price
+                    heappush(heap, ( k_stop + 1, price + next_price, next_node))
 
-
-## Bellman Ford
-class Solution:
-    def findCheapestPrice(
-        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
-    ) -> int:
-        costs = [float("inf")] * n
-
-        costs[src] = 0
-        for i in range(k + 1):
-            temp = costs.copy()
-            for start, end, price in flights:
-                if costs[start] != float("inf"):
-                    temp[end] = min(costs[start] + price, temp[end])
-            costs = temp
-        return costs[dst] if costs[dst] != float("inf") else -1
+        return visited[dst] if dst in visited else -1
+        
