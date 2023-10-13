f19479da064affe66fd46b2d1f5c12cf93eb1875
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-10-13 21:24:53 +0300

    Uploaded By Mirkusve

---
+++ permutation-in-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        checked = Counter(s1)
+        compared = defaultdict(int)
+        dummy = []
+        left = 0
+
+        for right in range(len(s2)):
+            compared[s2[right]] += 1
+            dummy.append(0)
+            while len(dummy) > len(s1):
+                compared[s2[left]] -= 1
+                if not compared[s2[left]]: 
+                    del(compared[s2[left]])
+                left += 1
+                dummy.pop()
+            
+            if compared == checked:
+                return True
+        
+        print(compared)
+        return False
